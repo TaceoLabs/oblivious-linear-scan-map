7c6316823d98b285a95f3c79a17068a18ae7590d
0xThemis <118167989+0xThemis@users.noreply.github.com>
2025-10-31 13:59:13 +0100

    refactor: modified build to reduce deps

--- bench.sh
+++ bench.sh
@@ -1,7 +1,5 @@
-cargo build --release --example bench-linear-scan
-
 ARGS="--seed 0 --runs 5 --num-items 100000"
 
-cargo run --release --example bench-linear-scan -- --config ./examples/configs/party1.toml $ARGS &
-cargo run --release --example bench-linear-scan -- --config ./examples/configs/party2.toml $ARGS &
-cargo run --release --example bench-linear-scan -- --config ./examples/configs/party3.toml $ARGS
+cargo run --release --features="local" --example bench-linear-scan -- --config ./examples/configs/party2.toml $ARGS &
+cargo run --release --features="local" --example bench-linear-scan -- --config ./examples/configs/party3.toml $ARGS & 
+cargo run --release --features="local" --example bench-linear-scan -- --config ./examples/configs/party1.toml $ARGS
